754ff13ed25d19faf3acd78adba60f64fa3e8dd9
MasoudKheradmand <104209505+MasoudKheradmandi@users.noreply.github.com>
2025-08-27 20:32:18 +0330

    add celery beat

--- app/tasks.py
+++ app/tasks.py
@@ -18,27 +18,40 @@ app.conf.update(
     beat_scheduler='celery.beat:PersistentScheduler',
     beat_schedule_filename='celerybeat-schedule',
     beat_max_loop_interval=300,
-    # beat_schedule={
-    #     'run-every-30-seconds': {
-    #         'task': 'save_user_data',
-    #         'schedule': 1000000.0,
-    #     }
-    # }
+    beat_schedule={
+        'run-every-30-seconds': {
+            'task': 'save_user_data',
+            'schedule': 10.0,
+        }
+    }
 )
 import requests
-from main import save_user
-
+from main import get_db,User
 @app.task(name='save_user_data')
 def save_user_data():
     limit = 100
     offset = 0
 
-    while True:
+    with get_db() as db:
+        offset = db.query(User).all()
+        if offset:
+            offset = offset[-1].user_id
+        else:
+            offset = 0
         payload = {
             "limit":limit,
             "offset": offset
         }
         response = requests.get('http://2.179.194.90/user_data/',params=payload)
-        offset += 100
-        # save_user(users_list=response.json())
-        return response.json()
+        users_to_add = [
+        User(
+            username=user_data.get("username"),
+            password=user_data.get("password"),
+            user_id=user_data.get("user_id")
+        )
+        for user_data in response.json()
+        ]
+    
+        db.add_all(users_to_add)
+        db.commit()
+        print(offset)
--- main.py
+++ main.py
@@ -14,12 +14,12 @@ class User(Base):
     password = Column(String, nullable=False)
     user_id =  Column(Integer,nullable=False)
 
-class Jwt(Base):
-    __tablename__="jwt"
-    id = Column(Integer, primary_key=True)
-    username =  Column(String, nullable=False)
-    jwt = Column(String, nullable=False)
-    user_id = Column(Integer,nullable=False)
+# class Jwt(Base):
+#     __tablename__="jwt"
+#     id = Column(Integer, primary_key=True)
+#     username =  Column(String, nullable=False)
+#     jwt = Column(String, nullable=False)
+#     user_id = Column(Integer,nullable=False)
 
 
 
@@ -27,8 +27,8 @@ engine = create_engine("sqlite:///local.db", echo=True)
 
 SessionLocal = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
-
-
+from contextlib import contextmanager
+@contextmanager
 def get_db():
     db = SessionLocal()
     try:
@@ -37,28 +37,40 @@ def get_db():
         db.close()
 
 
-def save_user(users_list: dict):
+def save_user():
     db_generator = get_db()
     db = next(db_generator)
-    
-    try:
-        users_to_add = [
-            User(
-                username=user_data.get("username"),
-                password=user_data.get("password"),
-                user_id=user_data.get("user_id")
-            )
-            for user_data in users_list
-        ]
-        
-        # تمام اشیاء را به صورت یکجا به Session اضافه کنید
-        db.add_all(users_to_add)
-        
-        db.commit()
-        print("All users saved successfully in a single batch!")
-        
-    except Exception as e:
-        db.rollback()
-        raise e
-    finally:
-        db.close()
+    limit = 100
+    offset = 0
+
+    while True:
+        payload = {
+            "limit":limit,
+            "offset": offset
+        }
+        response = requests.get('http://2.179.194.90/user_data/',params=payload)
+        offset += 100
+        try:
+            users_to_add = [
+                User(
+                    username=user_data.get("username"),
+                    password=user_data.get("password"),
+                    user_id=user_data.get("user_id")
+                )
+                for user_data in response.json()
+            ]
+            
+            # تمام اشیاء را به صورت یکجا به Session اضافه کنید
+            db.add_all(users_to_add)
+            
+            db.commit()
+            print("All users saved successfully in a single batch!")
+            
+        except Exception as e:
+            db.rollback()
+            raise e
+        finally:
+            db.close()
+
+if __name__ == "__main__":
+    save_user()
